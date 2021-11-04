da5a7134daa61b6a858ad39bd19a707048372007
neto <antonio.garciamn@gmail.com>
2021-11-04 13:49:04 -0300

    Virar Cartas

--- scripts.js
+++ scripts.js
@@ -1,15 +1,16 @@
 ncartas = prompt("Qual o numero de cartas?");
 
-if(ncartas%2 !== 0){
-    validNcartas();
-}
-
 function validNcartas(){
-    do{
-        ncartas = prompt("Escolha um numero par de cartas");
-    }while(ncartas%2 !== 0);
+    if(ncartas%2 !== 0){
+        do{
+            ncartas = prompt("Escolha um numero par de cartas");
+        }while(ncartas%2 !== 0);
+    }
 }
 
+do{ validNcartas()}while(validNcartas())
+
+ 
 function girar(sele){
     sele.classList.add("girar");
 }
