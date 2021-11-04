139860dfb1d72cb26b703459dfd3a5488d4a936d
neto <antonio.garciamn@gmail.com>
2021-11-04 17:08:51 -0300

    Repetição de Cartas

--- scripts.js
+++ scripts.js
@@ -1,36 +1,75 @@
-const ncartas = prompt("Qual o numero de cartas?");
+let ncartas = prompt("Qual o numero de cartas?");
 
-function validNcartas(){
-    if(ncartas%2 !== 0){
-        do{
-            ncartas = prompt("Escolha um numero par de cartas");
-        }while(ncartas%2 !== 0);
-    }
+while(ncartas<4 || ncartas>14 || ncartas%2 !== 0){
+    ncartas = prompt("Escolha um numero valido de cartas");
 }
 
-do{ validNcartas()}while(validNcartas());
+let fotos = [];
+fotos.push("<img src='imagens/unicornparrot.gif'>");
+fotos.push("<img src='imagens/bobrossparrot.gif'>");
+fotos.push("<img src='imagens/explodyparrot.gif'>");
+fotos.push("<img src='imagens/fiestaparrot.gif'>");
+fotos.push("<img src='imagens/metalparrot.gif'>");
+fotos.push("<img src='imagens/revertitparrot.gif'>");
+fotos.push("<img src='imagens/tripletsparrot.gif'>");
 
-function adicionarCarta(){
-    main = document.querySelector("main");
-    main.innerHTML = main.innerHTML + `
-    <article class="carta" data-identifier="card" onclick="girar(this)">
-    <div class="front-face face" data-identifier="back-face">
-        <img src="imagens/front.png">
-    </div>
-    <div class="back-face face" data-identifier="front-face">
-        <img src="imagens/bobrossparrot.gif" alt="não deu certo">
-    </div>
-    </article>`; 
+function CartasOriginais(num){
+    fotos.sort(comparador);
+     main = document.querySelector("main");
+     main.innerHTML = main.innerHTML + `
+     <article class="carta" data-identifier="card" onclick="girar(this)">
+     <div class="front-face face" data-identifier="back-face">
+         <img src="imagens/front.png">
+     </div>
+     <div class="back-face face" data-identifier="front-face">
+         ${fotos[num]}
+     </div>
+     </article>`;
 }
 
-let cont=0;
+let cont = 0;
 
-while(cont < ncartas){
-    adicionarCarta();
+while(cont < ncartas/2){
+    CartasOriginais(cont);
     cont++;
 }
 
 
+let copia = [];
+
+let cont2 = 0;
+
+while(cont2 < ncartas/2){
+    copia.push(fotos[cont2]);
+    cont2++;
+}
+
+
+function CartasCopiadas(num){
+    copia.sort(comparador);
+     main = document.querySelector("main");
+     main.innerHTML = main.innerHTML + `
+     <article class="carta" data-identifier="card" onclick="girar(this)">
+     <div class="front-face face" data-identifier="back-face">
+         <img src="imagens/front.png">
+     </div>
+     <div class="back-face face" data-identifier="front-face">
+         ${copia[num]}
+     </div>
+     </article>`;
+}
+
+let cont3 = 0;
+
+while(cont3 < ncartas/2){
+    CartasCopiadas(cont3);
+    cont3++;
+}
+
 function girar(sele){
     sele.classList.add("girar");
+}
+
+function comparador() { 
+	return Math.random() - 0.5; 
 }
