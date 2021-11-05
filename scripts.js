58d7c2e9b164b3087f8d76be3d1ec527174ffad8
neto <antonio.garciamn@gmail.com>
2021-11-05 13:38:20 -0300

    Embaralhar Cartas

--- scripts.js
+++ scripts.js
@@ -13,58 +13,31 @@ fotos.push("<img src='imagens/metalparrot.gif'>");
 fotos.push("<img src='imagens/revertitparrot.gif'>");
 fotos.push("<img src='imagens/tripletsparrot.gif'>");
 
-function CartasOriginais(num){
-    fotos.sort(comparador);
-     main = document.querySelector("main");
-     main.innerHTML = main.innerHTML + `
-     <article class="carta" data-identifier="card" onclick="girar(this)">
-     <div class="front-face face" data-identifier="back-face">
-         <img src="imagens/front.png">
-     </div>
-     <div class="back-face face" data-identifier="front-face">
-         ${fotos[num]}
-     </div>
-     </article>`;
-}
-
-let cont = 0;
-
-while(cont < ncartas/2){
-    CartasOriginais(cont);
-    cont++;
-}
-
-
 let copia = [];
 
-let cont2 = 0;
+function AdicionarCarta(array=[]){
+    array.sort(comparador);
+    let cont = 0;                       
+    do{
+        main = document.querySelector("main");
+        main.innerHTML = main.innerHTML + `
+        <article class="carta" data-identifier="card" onclick="girar(this)">
+        <div class="front-face face" data-identifier="back-face">
+            <img src="imagens/front.png">
+        </div>
+        <div class="back-face face" data-identifier="front-face">
+            ${array[cont]}
+        </div>
+        </article>`;
 
-while(cont2 < ncartas/2){
-    copia.push(fotos[cont2]);
-    cont2++;
+        copia.push(array[cont]);
+
+        cont++;
+    }while(cont < ncartas/2);
 }
 
-
-function CartasCopiadas(num){
-    copia.sort(comparador);
-     main = document.querySelector("main");
-     main.innerHTML = main.innerHTML + `
-     <article class="carta" data-identifier="card" onclick="girar(this)">
-     <div class="front-face face" data-identifier="back-face">
-         <img src="imagens/front.png">
-     </div>
-     <div class="back-face face" data-identifier="front-face">
-         ${copia[num]}
-     </div>
-     </article>`;
-}
-
-let cont3 = 0;
-
-while(cont3 < ncartas/2){
-    CartasCopiadas(cont3);
-    cont3++;
-}
+AdicionarCarta(fotos);
+AdicionarCarta(copia);
 
 function girar(sele){
     sele.classList.add("girar");
